0cad6a72c8db704b2331dca242170ebce39a9d69
haakh <hakbhavish@gmail.com>
2016-11-02 18:44:24 +0530

    Bootstrap ele

--- train/task7/myscript.js
+++ train/task7/myscript.js
@@ -1,62 +1,39 @@
-var App = new todoApp();
-var addNew = document.getElementById('addNewItem');
-function todoItem(){
-  this.caption;
-  this.isCompleted;
-  this.toggle = function () {
-    if(this.isCompleted === true){
-      this.isCompleted = false;
+$(document).ready(function(){
+var count =1;
+
+function addVal(){
+  var data = $('#addText').val();
+  $('#addNewItem').append('<li class = "checkbox list-group-item list-group-item-success" ><input type="checkbox" name="mycheck" id="ch' + count + '">'+ data  + '<button type="button" class ="btn btn-info btn-sm"id="rm' + count + '" >Remove' + '</li>');
+
+  $('#ch'+count).click(function(){
+
+    var $this = $(this);
+    if (this.checked) {
+
+        $this.parent().addClass('completed');
+    } else {
+        $this.parent().removeClass('completed');
     }
-    else {
-      this.isCompleted = true;
-    }
-  };
-  this.show = function(){
-    alert(this.caption);
-  }
-}
+  });
 
-function todoApp(){
-  this.todoCollection = [];
-  this.render = function () {
-    // var x = document.getElementById('start');
-    //insert html list below
-    addNew.innerHTML ="";
-    for (var i = 0; i < this.todoCollection.length; i++) {
 
-      addNew.innerHTML +="<div id="+i+"><input type='checkbox' onclick='line(this)'><span>"+this.todoCollection[i].caption+"</span><button onclick='removeData("+i+",this)'> Remove </button></div>";
 
-    }
+  $('#rm' + count).click(function(){
+    //Only remove when checkbox checked.
+    $(".checkbox input:checked").parent().remove();
+    // $(this).parent().remove();
+  });
+  count ++;
+};
 
-  }
-  this.addItem = function (data) {
-    var cap = new todoItem();
-    cap.caption = data;
-    cap.isCompleted = false;
-    this.todoCollection.push(cap);
-    // document.write(this.todoCollection[0].caption);
-  }
-}
-function line(e){
-  //textDecoration :line-through
-  // e.target.style.textDecoration="line-through";
-  if(e.checked){
-    document.getElementById(e.parentNode.id).style.textDecoration="line-through";
-  }
-  else {
-    document.getElementById(e.parentNode.id).style.textDecoration="none";
-  }
-  // document.write(e.parentNode.id);
-}
-function removeData(x,e){
-  //pop collection. using splice
-
-  App.todoCollection.splice(x,1);
-  App.render();
-}
-//EventListener
-document.getElementById("butt").addEventListener("click",function(){
-  var data = document.getElementById("newItem").value;
-  App.addItem(data);
-  App.render();
+$(function(){
+  $('#butt').on('click',addVal);
 });
+});
+
+
+
+
+// $('#rm' + count).click(function(){
+//   $(this).parent().remove();
+// });
